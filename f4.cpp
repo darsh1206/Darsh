e4099c199cd3e1ace66e51d834ff39eb4a3ebe1c
Trisha <trishavasoya@gmail.com>
2023-02-17 19:14:50 -0500

    Adding the condition in the for loop to get the lowest number with its index

--- f4.cpp
+++ f4.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main(){
     int a[10]={0,0,0,0,0,0,0,0,0,0};
     int index=0,min=INT_MAX;
+    printf("Enter 10 numbers: ");
+    for(int i=0;i<10;i++){
+        cin>>a[i];
+    }
     return 0;
 }
