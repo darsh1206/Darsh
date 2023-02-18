e7fd5cde20d8e561de22f3979ce2af57581a8c89
Trisha <trishavasoya@gmail.com>
2023-02-17 19:23:22 -0500

    Adding Main header comments with required details

--- f4.cpp
+++ f4.cpp
@@ -1,19 +1,26 @@
 
+//header files
 #include<stdio.h>
 #include<iostream>
 using namespace std;
 
+// main function
 int main(){
+    // declaring and initializing array and other variables
     int a[10]={0,0,0,0,0,0,0,0,0,0};
     int index=0,min=INT_MAX;
+
     printf("Enter 10 numbers: ");
+    // for loop to take input and get lowest number
     for(int i=0;i<10;i++){
         cin>>a[i];
+        // condition to find minimum number
         if(min>a[i]){
             min=a[i];
             index=i;
         }
     }
+    // printing lowest number and index 
     printf("\nThe lowest value is %d at index %d", min, index);
     return 0;
 }
